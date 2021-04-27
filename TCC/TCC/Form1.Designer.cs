ff88f7fabc40c74f29a9fd475c3bba920bb3a623
Giovani Sarchesi <giovanisarchesi@gmail.com>
2021-04-26 21:48:10 -0300

    Início desenvolvimento front e back
    Adicionado gif para usar no front, criação dos primeiros componentes e início desenvolvimento back (função para separar valores possíveis)

--- TCC/TCC/Form1.Designer.cs
+++ TCC/TCC/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TCC
 {
-    partial class Form1
+    partial class frmGeracao
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGeracao));
+            this.pctGif = new System.Windows.Forms.PictureBox();
+            this.btnGerarCombinacao = new System.Windows.Forms.Button();
+            this.pnlMensagem = new System.Windows.Forms.Panel();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.lblNomeVar = new System.Windows.Forms.Label();
+            this.lblValoresPossiveis = new System.Windows.Forms.Label();
+            this.txtNomeVar = new System.Windows.Forms.TextBox();
+            this.txtValoresPossiveis = new System.Windows.Forms.TextBox();
+            this.lblInstrucao = new System.Windows.Forms.Label();
+            this.btnAdicionaVariavel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctGif)).BeginInit();
+            this.pnlMensagem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pctGif
+            // 
+            this.pctGif.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctGif.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctGif.Image = ((System.Drawing.Image)(resources.GetObject("pctGif.Image")));
+            this.pctGif.Location = new System.Drawing.Point(0, 0);
+            this.pctGif.Name = "pctGif";
+            this.pctGif.Size = new System.Drawing.Size(541, 583);
+            this.pctGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pctGif.TabIndex = 0;
+            this.pctGif.TabStop = false;
+            this.pctGif.Visible = false;
+            // 
+            // btnGerarCombinacao
+            // 
+            this.btnGerarCombinacao.Location = new System.Drawing.Point(12, 498);
+            this.btnGerarCombinacao.Name = "btnGerarCombinacao";
+            this.btnGerarCombinacao.Size = new System.Drawing.Size(517, 35);
+            this.btnGerarCombinacao.TabIndex = 1;
+            this.btnGerarCombinacao.Text = "Gerar Combinação";
+            this.btnGerarCombinacao.UseVisualStyleBackColor = true;
+            this.btnGerarCombinacao.Click += new System.EventHandler(this.btnGerarCombinacao_Click);
+            // 
+            // pnlMensagem
+            // 
+            this.pnlMensagem.Controls.Add(this.lblMensagem);
+            this.pnlMensagem.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlMensagem.Location = new System.Drawing.Point(0, 539);
+            this.pnlMensagem.Name = "pnlMensagem";
+            this.pnlMensagem.Size = new System.Drawing.Size(541, 44);
+            this.pnlMensagem.TabIndex = 2;
+            this.pnlMensagem.Visible = false;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(106, 11);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(329, 24);
+            this.lblMensagem.TabIndex = 0;
+            this.lblMensagem.Text = "Gerando combinações, aguarde...";
+            this.lblMensagem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMensagem.Visible = false;
+            // 
+            // lblNomeVar
+            // 
+            this.lblNomeVar.AutoSize = true;
+            this.lblNomeVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeVar.Location = new System.Drawing.Point(15, 15);
+            this.lblNomeVar.Name = "lblNomeVar";
+            this.lblNomeVar.Size = new System.Drawing.Size(100, 17);
+            this.lblNomeVar.TabIndex = 3;
+            this.lblNomeVar.Text = "Nome Variável";
+            // 
+            // lblValoresPossiveis
+            // 
+            this.lblValoresPossiveis.AutoSize = true;
+            this.lblValoresPossiveis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValoresPossiveis.Location = new System.Drawing.Point(15, 63);
+            this.lblValoresPossiveis.Name = "lblValoresPossiveis";
+            this.lblValoresPossiveis.Size = new System.Drawing.Size(118, 17);
+            this.lblValoresPossiveis.TabIndex = 4;
+            this.lblValoresPossiveis.Text = "Valores possíveis";
+            // 
+            // txtNomeVar
+            // 
+            this.txtNomeVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeVar.Location = new System.Drawing.Point(15, 36);
+            this.txtNomeVar.Name = "txtNomeVar";
+            this.txtNomeVar.Size = new System.Drawing.Size(514, 23);
+            this.txtNomeVar.TabIndex = 5;
+            // 
+            // txtValoresPossiveis
+            // 
+            this.txtValoresPossiveis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValoresPossiveis.Location = new System.Drawing.Point(15, 84);
+            this.txtValoresPossiveis.Name = "txtValoresPossiveis";
+            this.txtValoresPossiveis.Size = new System.Drawing.Size(514, 23);
+            this.txtValoresPossiveis.TabIndex = 6;
+            // 
+            // lblInstrucao
+            // 
+            this.lblInstrucao.AutoSize = true;
+            this.lblInstrucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstrucao.Location = new System.Drawing.Point(139, 67);
+            this.lblInstrucao.Name = "lblInstrucao";
+            this.lblInstrucao.Size = new System.Drawing.Size(265, 13);
+            this.lblInstrucao.TabIndex = 7;
+            this.lblInstrucao.Text = "(Separe os valores com ponto e vírgula (;). Ex: A; B; C)";
+            // 
+            // btnAdicionaVariavel
+            // 
+            this.btnAdicionaVariavel.Location = new System.Drawing.Point(340, 113);
+            this.btnAdicionaVariavel.Name = "btnAdicionaVariavel";
+            this.btnAdicionaVariavel.Size = new System.Drawing.Size(189, 23);
+            this.btnAdicionaVariavel.TabIndex = 8;
+            this.btnAdicionaVariavel.Text = "Adicionar Variável";
+            this.btnAdicionaVariavel.UseVisualStyleBackColor = true;
+            this.btnAdicionaVariavel.Click += new System.EventHandler(this.btnAdicionaVariavel_Click);
+            // 
+            // frmGeracao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(541, 583);
+            this.Controls.Add(this.btnAdicionaVariavel);
+            this.Controls.Add(this.lblInstrucao);
+            this.Controls.Add(this.txtValoresPossiveis);
+            this.Controls.Add(this.txtNomeVar);
+            this.Controls.Add(this.lblValoresPossiveis);
+            this.Controls.Add(this.lblNomeVar);
+            this.Controls.Add(this.pnlMensagem);
+            this.Controls.Add(this.btnGerarCombinacao);
+            this.Controls.Add(this.pctGif);
+            this.Name = "frmGeracao";
+            this.Text = "Gerador de Combinações";
+            ((System.ComponentModel.ISupportInitialize)(this.pctGif)).EndInit();
+            this.pnlMensagem.ResumeLayout(false);
+            this.pnlMensagem.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctGif;
+        private System.Windows.Forms.Button btnGerarCombinacao;
+        private System.Windows.Forms.Panel pnlMensagem;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label lblNomeVar;
+        private System.Windows.Forms.Label lblValoresPossiveis;
+        private System.Windows.Forms.TextBox txtNomeVar;
+        private System.Windows.Forms.TextBox txtValoresPossiveis;
+        private System.Windows.Forms.Label lblInstrucao;
+        private System.Windows.Forms.Button btnAdicionaVariavel;
     }
 }
 
--- TCC/TCC/Form1.cs
+++ TCC/TCC/Form1.cs
@@ -1,20 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TCC
 {
-    public partial class Form1 : Form
+    public partial class frmGeracao : Form
     {
-        public Form1()
+
+        public List<string> valores = new List<string>();
+
+        public frmGeracao()
         {
             InitializeComponent();
+        }
+
+        private void btnGerarCombinacao_Click(object sender, EventArgs e)
+        {
+            MostraGif();
+        }
+
+        private void btnAdicionaVariavel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtNomeVar.Text))
+                {
+                    throw new Exception("O nome da variável deve ser informado.");
+                }
+                //If para verificar se os valores informados pelo usuário são válidos
+                //Ou seja: não vazio e com ponto e vírgula (;)
+                if (!string.IsNullOrEmpty(txtValoresPossiveis.Text) && txtValoresPossiveis.Text.Contains(";"))
+                {
+                    //If para verificar se o último caracter informado é ponto e virgula
+                    if(txtValoresPossiveis.Text.Substring(txtValoresPossiveis.Text.Length - 1, 1) == ";")
+                    {
+                        //Se for verdadeiro, o ultimo caracter é removido antes de realizar o split para não adicionar vazio na lista de valores possíveis
+                        valores = txtValoresPossiveis.Text.Remove(txtValoresPossiveis.Text.Length - 1, 1).Split(';').ToList();
+                    }
+                        //Se falso, é realizado apenas o split
+                    else
+                    {
+                        valores = txtValoresPossiveis.Text.Split(';').ToList();
+                    }
+                }
+                else
+                {
+                    throw new Exception("O campo \"Valores Possíveis\" não está no formato correto!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //Funções
+
+        //Função para esconder os componentes e mostrar o gif e mensagem para o usuário.
+        public void MostraGif()
+        {
+            pctGif.Visible = true;
+            pnlMensagem.Visible = true;
+            lblMensagem.Visible = true;
+            btnGerarCombinacao.Visible = false;
+            txtNomeVar.Visible = false;
+            txtValoresPossiveis.Visible = false;
+            lblInstrucao.Visible = false;
+            lblNomeVar.Visible = false;
+            lblValoresPossiveis.Visible = false;
+            btnAdicionaVariavel.Visible = false;
         }
     }
 }
